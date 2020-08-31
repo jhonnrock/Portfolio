139b47c836c725686d6026d7a7c8eb224f35b9bb
jhonnrock <jhonn7890@gmail.com>
2020-08-31 14:38:11 -0400

    Add to my repo

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,21 @@ collapse.addEventListener('click',()=>{
     nav.style.background=`#070022`;
    
 });
+const nameuser=document.querySelector('.name-user');
+console.log(nameuser);
 const nav =document.querySelector('.view-menu');
 window.addEventListener("scroll", function () {
     window.scrollY > 100 && (nav.style.background = `rgba(80,35,146,0.9)`);
     window.scrollY < 100 && (nav.style.background = `transparent`);
+    if(window.scrollY > 100 && (nav.style.background)){
+
+        nameuser.classList.add('inative');
+    }
+    else{
+        nameuser.classList.remove('inative');
+    }
+
+
   });
 const count=0;
 const links=document.querySelector('.linkdata');
